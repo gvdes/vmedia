6e2831d1e42c8f09cefced9680b279adcb28997f
hugomers <hugomo0897@gmail.com>
2023-06-02 17:46:07 -0600

    se agrega el cors middleware para poder conectar espero que sirv

--- app/Http/Middleware/CorsMidleware.php
+++ app/Http/Middleware/CorsMidleware.php
@@ -16,20 +16,28 @@ class CorsMidleware
      */
     public function handle(Request $request, Closure $next)
     {
-        $headers = [
-            'Access-Control-Allow-Origin'      => '192.168.10.189:2202',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Accept, Authorization, X-Requested-With, Application, ip'
-        ];
-        if ($request->isMethod('OPTIONS')){
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        }
-        $response = $next($request);
-        foreach($headers as $key => $value){
-            $response->headers->set($key, $value);
-        }
-        return $response;
+        // $headers = [
+        //     'Access-Control-Allow-Origin'      => '*',
+        //     'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+        //     'Access-Control-Allow-Credentials' => 'true',
+        //     'Access-Control-Max-Age'           => '86400',
+        //     'Access-Control-Allow-Headers'     => 'Content-Type, Accept, Authorization, X-Requested-With, Application'
+        // ];
+        // if ($request->isMethod('OPTIONS')){
+        //     return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        // }
+        // $response = $next($request);
+        // foreach($headers as $key => $value){
+        //     $response->headers->set($key, $value);
+        // }
+        // return $response;
+        return $next($request)
+        //Url a la que se le dará acceso en las peticiones
+       ->header("Access-Control-Allow-Origin", "*")
+       //Métodos que a los que se da acceso
+       ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+       //Headers de la petición
+       ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+
     }
 }
